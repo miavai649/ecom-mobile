320356b7a0fee4e282a8ad03dd06e91e52bb8283
miavai649 <mahmudulhaquenoor@gmail.com>
2024-12-17 10:47:33 +0600

    installed gluestack properly

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,13 @@
 import { Stack } from 'expo-router'
+import { GluestackUIProvider } from './../components/ui/gluestack-ui-provider/index'
+import '@/global.css'
 
 const RootLayout = () => {
-  return <Stack />
+  return (
+    <GluestackUIProvider>
+      <Stack />
+    </GluestackUIProvider>
+  )
 }
 
 export default RootLayout
--- app/index.tsx
+++ app/index.tsx
@@ -2,13 +2,17 @@ import { FlatList, Text } from 'react-native'
 import React from 'react'
 import products from '../assets/products.json'
 import ProductListItem from '../components/ProductListItem'
+import { Button, ButtonText } from '@/components/ui/button'
 
 const Home = () => {
   return (
-    <FlatList
-      data={products}
-      renderItem={({ item }) => <ProductListItem item={item} />}
-    />
+    // <FlatList
+    //   data={products}
+    //   renderItem={({ item }) => <ProductListItem item={item} />}
+    // />
+    <Button size='md' variant='outline' action='positive'>
+      <ButtonText>Hello World!</ButtonText>
+    </Button>
   )
 }
 
